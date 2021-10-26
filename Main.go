45340b06e1e9b6a24f0cbbcf888e2d41ce9aecc8
Sid.Zhang <sid.zhang@asurion.com>
2021-10-26 23:02:29 +0900

    No.77 5%

--- Main.go
+++ Main.go
@@ -1,10 +1,10 @@
 package main
 
-import "LeetCode/regular"
+import "leetcode-daily/regular"
 
 func main() {
 	//y2021m09.Call05()
 	//most_popular.Call102()
 	//most_popular.CallT05()
-	regular.T297()
+	regular.T77()
 }
--- regular/T77.go
+++ regular/T77.go
@@ -3,10 +3,25 @@ package regular
 import "fmt"
 
 func combine(n int, k int) [][]int {
-	return nil
+	queue := [][]int{{}}
+	results := [][]int{}
+	for i := 1; i <= n; i++ {
+		size := len(queue)
+		for j := 0; j < size; j++ {
+			copied := make([]int, len(queue[j]))
+			copy(copied, queue[j])
+			copied = append(copied, i)
+			if len(copied) == k {
+				results = append(results, copied)
+			} else if len(copied) < k {
+				queue = append(queue, copied)
+			}
+		}
+	}
+	return results
 }
 
 func T77() {
-	res := combine(1, 1)
+	res := combine(20, 10)
 	fmt.Println(res)
 }
